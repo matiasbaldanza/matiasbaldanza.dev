0006fa423c56f7f92dac54d6229676289bd60bf9
Matias Baldanza <matiasbaldanza@gmail.com>
2026-07-07 21:56:14 -0300

    chore: disable Astro toolbar on dev mode while debugging issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://matiasbaldanza.dev',
+  devToolbar: {
+    enabled: false,
+  },
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 4321,
   },
